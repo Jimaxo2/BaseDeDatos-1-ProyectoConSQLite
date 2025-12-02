743407cb4a186498f14f51a6e3e11c61a4ea3244
Jimaxo2 <jimaxogamer@gmail.com>
2025-12-01 18:43:06 -0700

    Se removio clase en desuso

--- DBConexion.cs
+++ DBConexion.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BaseDeDatos_SQLLite
 {
